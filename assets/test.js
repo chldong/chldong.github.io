f60b4fdf7d8a934db91ff19455cd83df8d597c3c
chldong <cuihailiang@gmail.com>
2020-07-01 22:38:53 +0800

    test javascript

--- assets/test.js
+++
@@ -1,1 +0,0 @@
-<script type="text/javascript" src="//rf.revolvermaps.com/0/0/1.js?i=5uivt9qif4h&amp;s=220&amp;m=0&amp;v=false&amp;r=false&amp;b=000000&amp;n=false&amp;c=ff0000" async="async"></script>
